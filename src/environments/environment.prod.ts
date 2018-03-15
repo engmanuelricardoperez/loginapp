ccf3c98aebe14fd35039f388d5b8c321cc2bd6af
engmanuelricardoperez <manuelricardo.perez@hotmail.com>
2018-03-15 12:00:51 -0500

    Creado el SERVICIO AUTH y registro de usuario por EMAIL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyDcFXVx1_3UvDLtoWaP9wt8gNAUqx7uQ0Y",
+    authDomain: "apptrial-a34d0.firebaseapp.com",
+    databaseURL: "https://apptrial-a34d0.firebaseio.com",
+    projectId: "apptrial-a34d0",
+    storageBucket: "apptrial-a34d0.appspot.com",
+    messagingSenderId: "93145970935"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyDcFXVx1_3UvDLtoWaP9wt8gNAUqx7uQ0Y",
+    authDomain: "apptrial-a34d0.firebaseapp.com",
+    databaseURL: "https://apptrial-a34d0.firebaseio.com",
+    projectId: "apptrial-a34d0",
+    storageBucket: "apptrial-a34d0.appspot.com",
+    messagingSenderId: "93145970935"
+  }
 };
